4c81dbaf065da4eb8b12ebedbc9efb8b6aa5de96
Romain Bioteau <romain.bioteau@bonitasoft.com>
2020-10-12 09:54:12 +0200

    feat(project) update project layout (#10)

--- src/test/java/org/bonitasoft/connectors/cmis/cmisclient/CMISParametersValidatorTest.java
+++ src/test/java/org/bonitasoft/connectors/cmis/cmisclient/CMISParametersValidatorTest.java
@@ -1,43 +1,58 @@
+/*
+ * Copyright (C) 2009 - 2020 Bonitasoft S.A.
+ * Bonitasoft, 32 rue Gustave Eiffel - 38000 Grenoble
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2.0 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 package org.bonitasoft.connectors.cmis.cmisclient;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasItems;
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
+class CMISParametersValidatorTest {
 
-public class CMISParametersValidatorTest {
     @Test
-    public void testValidateCommonParameters() throws Exception {
-        final CMISParametersValidator cmisParametersValidator = new CMISParametersValidator(Collections.<String, Object>emptyMap());
-        assertThat(cmisParametersValidator.validateCommonParameters(), hasItems(
+    void testValidateCommonParameters() throws Exception {
+        final CMISParametersValidator cmisParametersValidator = new CMISParametersValidator(
+                Collections.<String, Object> emptyMap());
+        assertThat(cmisParametersValidator.validateCommonParameters()).contains(
                 "Binding type is not set",
                 "Repository must be set",
                 "Username is not set",
-                "Password is not set"
-        ));
+                "Password is not set");
     }
 
     @Test
-    public void testBinding() throws Exception {
-        final Map<String,Object> parameters = new HashMap<String, Object>();
+    void testBinding() throws Exception {
+        final Map<String, Object> parameters = new HashMap<String, Object>();
         parameters.put("binding_type", "b");
 
         final CMISParametersValidator cmisParametersValidator = new CMISParametersValidator(parameters);
         assertThat(cmisParametersValidator.validateCommonParameters(), hasItem(
-                "Binding type should be either atompub or webservices"
-        ));
+                "Binding type should be either atompub or webservices"));
     }
 
     @Test
-    public void testValidateSpecificParameters() throws Exception {
-        final Map<String,Object> parameters = new HashMap<String, Object>();
+    void testValidateSpecificParameters() throws Exception {
+        final Map<String, Object> parameters = new HashMap<String, Object>();
         parameters.put("subfolder_name", null);
         parameters.put("folder_path", null);
         parameters.put("document_path", null);
@@ -49,7 +64,7 @@ public class CMISParametersValidatorTest {
 
         final CMISParametersValidator cmisParametersValidator = new CMISParametersValidator(parameters);
 
-        assertThat(cmisParametersValidator.validateSpecificParameters(), hasItems(
+        assertThat(cmisParametersValidator.validateSpecificParameters()).contains(
                 "Document must be set",
                 "Folder path must be set",
                 "Destination name must be set",
@@ -58,14 +73,13 @@ public class CMISParametersValidatorTest {
                 "Sub folder must be set",
                 "Document path must be set",
                 "Document ID must be set",
-                "Query must be set"
-        ));
+                "Query must be set");
     }
 
     @Test
-    public void testNoSpecificParameters() throws Exception {
+    void testNoSpecificParameters() throws Exception {
         final Map<String, Object> parameters = new HashMap<String, Object>();
         final CMISParametersValidator cmisParametersValidator = new CMISParametersValidator(parameters);
-        assertThat(cmisParametersValidator.validateSpecificParameters().size(), is(0));
+        assertThat(cmisParametersValidator.validateSpecificParameters()).isEmpty();
     }
 }
